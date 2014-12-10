eeceb0876945a87ed5a9f652ac0f0eb06e74331b
satbeka <satbeka@gmail.com>
2014-12-10 21:57:09 +0600

    101214

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -1,8 +1,18 @@
 package model;
 
 
-public class Product {
 
+import java.sql.Date;
+import java.util.Comparator;
 
+public class Product implements Comparator<Product>{
+    private long id;
+    private String name;
+    private int active;
+    private Date insertDate;
 
+    @Override
+    public int compare(Product o1, Product o2) {
+        return o1.insertDate.compareTo(o2.insertDate);
+    }
 }
